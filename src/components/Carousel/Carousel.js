4b377803fcf4c144eddb2fdbc8179ec935e67ba5
Adam Lee <adam123dublin@gmail.com>
2021-01-26 15:32:26 +0000

    Update Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -112,7 +112,7 @@ class Carousel extends Component {
 
   getActiveItem = (offset) => {
     const center = this.getCenter(offset)
-    const centerOffset = 20
+    const centerOffset = 0
 
     for (let i = 0; i < this.positions.length; i += 1) {
       const { start, end } = this.positions[i]
